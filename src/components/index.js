0abad35b88bf5c2dca4e4831b843f4df137fb22b
hareesh-r <hareeshprogrammer@gmail.com>
2021-09-27 21:09:03 +0530

    feat :Created necessary Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,6 @@
 export { default as Navbar} from './Navbar'
+export { default as CryptoDetails} from './CryptoDetails'
+export { default as Cryptocurrencies} from './Cryptocurrencies'
+export { default as News} from './News'
+export { default as Homepage} from './Homepage'
+export { default as Exchanges} from './Exchanges'
