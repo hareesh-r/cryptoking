edd0fea11ded7e8b7592dcab3fc144bdcd746f43
hareesh-r <hareeshprogrammer@gmail.com>
2021-10-17 23:17:22 +0530

    feat : Added cryptoNewsApi

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { cryptoApi } from '../services/cryptoApi';
+import { cryptoNewsApi } from '../services/cryptoNewsApi';
 
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
   },
 });
