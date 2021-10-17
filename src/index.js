076e60d437ddc120878ba54573f435c1244e2769
hareesh-r <hareeshprogrammer@gmail.com>
2021-10-17 19:30:39 +0530

    feat : Added Redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import store from './app/store';
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css'
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+          <App />
+    </Provider>
     </Router>,
   document.getElementById('root')
 );
