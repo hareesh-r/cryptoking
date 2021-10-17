9d3cb620f4a4ce1dfe6f41044cf0204f9b9dc6eb
hareesh-r <hareeshprogrammer@gmail.com>
2021-10-17 19:29:52 +0530

    feat : Added Crypto currency Row , Col and Card

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
+import millify from 'millify'
+import { Link } from 'react-router-dom'
+import {Card,Row,Col,Input} from 'antd'
+
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = () => {
+    const { data:cryptosList , isFetching } = useGetCryptosQuery();
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+    console.log(cryptos)
     return (
-        <div>
-            Cryptocurrencies
-        </div>
+        <>
+        <Row gutters={[32,32]} className="crypto-card-container">
+            {
+                cryptos.map((currency)=>(
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+                        <Link to={`/crypto/${currency.id}`}>
+                            <Card
+                                title={`${currency.rank}. ${currency.name}`}
+                                extra={<img className="crypto-image" src={currency.iconUrl}/>}
+                                hoverable
+                            >
+                                <p>Price: {millify(currency.price)}</p>
+                                <p>Market Cap: {millify(currency.marketCap)}</p>
+                                <p>Daily Change: {millify(currency.change)}</p>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))
+            }
+        </Row>
+        </>
     )
 }
 
