b0b42ff2be9ccb97688c29e59c2fe809fc6f30ff
hareesh-r <hareeshprogrammer@gmail.com>
2021-10-17 19:28:03 +0530

    feat : Added Redux API

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '1c696eeb37mshe1242fffa274dc7p1cdf6ajsn1a94da8542a6'
+};
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest(`/coins`),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery , } = cryptoApi;
