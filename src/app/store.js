085009f168984051fd5b4407d0bb50d7f801b9b7
hareesh-r <hareeshprogrammer@gmail.com>
2021-10-17 19:27:13 +0530

    feat : Added Reducer

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { cryptoApi } from '../services/cryptoApi';
+
+
+export default configureStore({
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+  },
+});
